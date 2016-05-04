3453e9a4f9f4e61d64da43ede3d15c44a7a43cc7
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-04 10:01:44 +0100

    Passing create links test now

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -2,8 +2,8 @@ feature 'Create links' do
 
   scenario 'users can create a new link' do
     visit '/links/new'
-    fill_in 'url', :with => "http://www.aol.com"
-    fill_in 'title', :with => "AOL"
+    fill_in 'url', with: "http://www.aol.com"
+    fill_in 'title', with: "AOL"
     click_button 'Submit'
     expect(page).to have_content "AOL"
   end
