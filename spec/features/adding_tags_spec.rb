92f2024df0b2d37f1e356a9b2da5fd586d9f739b
Noah Pollock <pollocknoah@gmail.com>
2016-05-05 10:24:47 +0100

    multiple tags failing

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -2,14 +2,24 @@ feature 'Adding tags' do
 
   scenario 'I can add a single tag to a new link' do
     visit '/links/new'
-    fill_in('title',   with: 'Apple')
-    fill_in('url',     with: 'http://www.apple.co.uk/' )
-    fill_in( 'tags',    with: 'tech')
+    fill_in 'title',   with: 'Apple'
+    fill_in 'url',     with: 'http://www.apple.co.uk/'
+    fill_in 'tags',    with: 'tech'
 
-    click_button('Add')
+    click_button 'Add'
     link = Link.first
     expect(link.tags.map(&:name)).to include('tech')
   end
 
-  
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'title',   with: 'Sean Paul'
+    fill_in 'url',     with: 'http://www.seanpaul.com'
+    fill_in 'tags',    with: 'tech rubbish'
+
+    click_button 'Add'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('tech', 'rubbish')
+  end
+
 end
