f17356ca209cc9b2e61140552fd75ac78faa258c
Alex Robertson <lex1989@gmail.com>
2016-05-06 17:07:05 +0100

    Added flash

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,9 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-require_relative 'data_mapper_setup'
 require 'sinatra/flash'
+require_relative 'data_mapper_setup'
+
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
